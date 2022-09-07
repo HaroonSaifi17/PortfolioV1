5590ce002d7940c5121a900a74a31f9dc3679e87
Mohd Haroon <haroonsaifi2001@gmail.com>
2022-09-08 01:42:37 +0530

    Some Twicks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { HeroSectionComponent } from './components/sections/hero-section/hero-section.component';
-import { AboutMeComponent } from './components/sections/about-me/about-me.component';
-import { ContactComponent } from './components/sections/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { GithubComponent } from './components/icons/github.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NavBarComponent } from './components/nav-bar/nav-bar.component'
+import { HeroSectionComponent } from './components/sections/hero-section/hero-section.component'
+import { AboutMeComponent } from './components/sections/about-me/about-me.component'
+import { ContactComponent } from './components/sections/contact/contact.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { GithubComponent } from './components/icons/github.component'
+import { LogoComponent } from './components/icons/logo.component'
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,13 +18,11 @@ import { GithubComponent } from './components/icons/github.component';
     AboutMeComponent,
     ContactComponent,
     FooterComponent,
-    GithubComponent
+    GithubComponent,
+    LogoComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/icons/logo.component.ts
+++ src/app/components/icons/logo.component.ts
@@ -1,8 +1,38 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-logo',
-  template: ``,
+  template: `
+    <svg
+      id="logo"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 84 96"
+      height="40px"
+      width="40px"
+    >
+      <title>Logo</title>
+      <g transform="translate(-8.000000, -2.000000)">
+        <g transform="translate(11.000000, 5.000000)">
+          <path
+            fill="transparent"
+            stroke="#64ffda"
+            stroke-width="5"
+            d="M31.4,30v30v-15h15v-15v30"
+          />
+          <polygon
+            fill="transparent"
+            id="Shape"
+            stroke="#64ffda"
+            stroke-width="5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            points="39 0 0 22 0 67 39 90 78 68 78 23"
+          />
+        </g>
+      </g>
+    </svg>
+  `,
   styles: ['svg:hover{stroke:green}'],
 })
 export class LogoComponent {}
